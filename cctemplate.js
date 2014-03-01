d1530343fe9204e40a5a09e2b73e015000c72f5f
clash1994 <mjcharz4ever@gmail.com>
2014-03-01 22:01:58 +0530

    fixed mongo issues

--- cctemplate.js
+++ cctemplate.js
@@ -4,39 +4,48 @@ var app     = express();
 app.use(express.bodyParser());
 
 app.post('/', function(req, res) {
-  //res.send('You sent the name "' + req.body.name + '".');
   var username=req.body.name;
-  var password=req.body.pass;
-  var Db = require('mongodb').Db,
-    MongoClient = require('mongodb').MongoClient,
-    Server = require('mongodb').Server,
-    ReplSetServers = require('mongodb').ReplSetServers,
-    ObjectID = require('mongodb').ObjectID,
-    Binary = require('mongodb').Binary,
-    GridStore = require('mongodb').GridStore,
-    Grid = require('mongodb').Grid,
-    Code = require('mongodb').Code,
-    BSON = require('mongodb').pure().BSON,
-    assert = require('assert');
-  
-  var mongoclient = new MongoClient(new Server("localhost", 27017), {native_parser: true});
+  var password=req.body.password;
 
-  // Open the connection to the server
-  mongoclient.open(function(err, mongoclient) {
+  var MongoClient = require('mongodb').MongoClient
+    , format = require('util').format;
 
-  
-  
-  var db = mongoclient.db("mydb");
-  var collection = db.collection('testData');
+  MongoClient.connect('mongodb://localhost:6331/mydb', function(err, db) {
+    if(err) throw err;
 
+    var collection = db.collection('testData');
+    
+
+    
+
+
+      collection.find({name:username}).toArray(function(err, results) {
+        if(results[0]){
+          if(results[0]['pass']!=password){
+
+            res.redirect(__dirname + "/./login.html");
+          }
+
+          else{
+            res.redirect(__dirname + "/./template.html");
+          }
+
+        }
+
+        else{
+          res.redirect(__dirname + "/./login.html");
+        }
+
+        
+        db.close();
+      });
+
+      
+    
+  })
   
-  collection.findOne({name:username}).pass;
-  if(pass==password){
-    res.send("yola");
-    console.log("yo");
-  }
-   
-    });
+  
+
 });
 
 
